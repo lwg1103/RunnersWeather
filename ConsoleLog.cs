6c0dc3c1cb6a8e86b47e829fd85a3fa001f09d86
Dominik Czajka <lwg1103@wp.pl>
2019-12-20 23:28:30 +0100

    console log added

---
+++ ConsoleLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunnersWeather
+{
+    public class ConsoleLog
+    {
+        private TextBox textBox;
+        public ConsoleLog(TextBox textBox) => this.textBox = textBox;
+        
+        public void AddEntry(string message)
+        {
+            textBox.Text += System.Environment.NewLine + message;
+        }
+
+        public void Clear()
+        {
+            textBox.Text = "";
+        }
+    }
+}
