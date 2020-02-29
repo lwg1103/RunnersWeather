21136b81a17ac366538d48b7666ba84365788344
Dominik Czajka <lwg1103@wp.pl>
2020-02-29 14:21:33 +0100

    clothes advisor - work in progress

---
+++ RunnersWeather/Clothes/ClothesAdvisor.cs
@@ -0,0 +1,19 @@
+﻿using RunnersWeather.Conditions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunnersWeather.Clothes
+{
+    public class ClothesAdvisor : IClothesAdvisor
+    {
+        public List<ClothesPiece> SelectRecommendedClothesForConditions(WeatherConditions conditions, ClothesContext context)
+        {
+            var result = new List<ClothesPiece>();
+
+            return result;
+        }
+    }
+}
---
+++ RunnersWeatherTests/Clothes/ClothesAdvisorTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using RunnersWeather.Conditions;
+using System.Linq;
+
+namespace RunnersWeather.Clothes.Tests
+{
+    [TestClass()]
+    public class ClothesAdvisorTests
+    {
+        ClothesAdvisor TestSubject = new ClothesAdvisor();
+        [TestMethod()]
+        public void SelectRecommendedClothesForConditionsReturnsClothesList()
+        {
+            var clothes = TestSubject.SelectRecommendedClothesForConditions(new WeatherConditions(), new ClothesContext());
+            Assert.IsInstanceOfType(clothes, typeof(List<ClothesPiece>));
+        }
+        [TestMethod()]
+        public void SelectRecommendedClothesForConditionsReturnsClothsThatMatchesConditions()
+        {
+            var clothes = TestSubject.SelectRecommendedClothesForConditions(GetWeatherConditions(), new ClothesContext());
+
+            foreach (var piece in GetExpectedClothesPieces())
+            {
+                CollectionAssert.Contains(clothes, piece);
+            }
+        }
+        [TestMethod()]
+        public void SelectRecommendedClothesForConditionsReturnsEmptyListIfNothingMatchesConditions()
+        {
+            var clothes = TestSubject.SelectRecommendedClothesForConditions(new WeatherConditions(), new ClothesContext());
+            Assert.AreEqual(0, clothes.Count);
+        }
+
+        private WeatherConditions GetWeatherConditions()
+        {
+            return new WeatherConditions()
+            {
+                HUMIDITY = 15.0f,
+                PM25 = 35.0f,
+                TEMPERATURE = 4.0f
+            };
+        }
+
+        private List<ClothesPiece> GetExpectedClothesPieces()
+        {
+            var clothes = new ClothesContext().ClothesPiecesDbSet;
+
+            var cap = clothes.Where(c => c.Name == "Czapka Gruba").FirstOrDefault();
+            var mask = clothes.Where(c => c.Name == "Maska Antysmogowa").FirstOrDefault();
+
+            return new List<ClothesPiece> { cap, mask };
+        }
+    }
+}
